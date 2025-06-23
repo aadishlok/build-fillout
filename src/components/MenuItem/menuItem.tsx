f9fd21de9eb4990e6c58a4798086d02ca6eda133
aadishlok <aadishlok9@gmail.com>
2025-06-23 00:20:31 -0400

    refactored menu item classes

--- src/components/MenuItem/menuItem.tsx
+++ src/components/MenuItem/menuItem.tsx
@@ -7,6 +7,41 @@ const MenuItem = ({itemId, Icon, text, isSelected, onSelect}: IMenuItem) => {
 
     const containerRef = useRef<HTMLDivElement>(null);
 
+    const defaultClasses = `
+        flex-grow 
+        min-w-[84px]
+        h-[32px] 
+        py-[4px] 
+        px-[10px] 
+        rounded-[8px] 
+        flex 
+        items-center 
+        text-sm 
+        font-medium 
+        cursor-pointer 
+        transition-all 
+        outline-none 
+        bg-[#9DA4B226] 
+        text-gray-800 
+        border-[0.5px] 
+        border-transparent 
+        justify-center
+    `;
+
+    const selectedStateClasses = `
+        bg-white 
+        border-[#E1E1E1] 
+        justify-between 
+        text-[#1C1C1C]
+    `;
+
+    const focusStateClasses = `
+        hover:bg-[#9DA4B259]
+        group-focus-within:bg-white
+        group-focus-within:shadow-[0px_1px_3px_0px_rgba(0,0,0,0.04),_0px_1px_1px_0px_rgba(0,0,0,0.02),_0px_0px_0px_1.5px_rgba(47,114,226,0.25)]
+        group-focus-within:justify-center
+    `;
+
     return (
         <div
             key={itemId}
@@ -15,40 +50,7 @@ const MenuItem = ({itemId, Icon, text, isSelected, onSelect}: IMenuItem) => {
             className="group"
             onClick={onSelect} // Handle click to set this page as selected
         >
-            <div
-                className={`
-                    flex-grow 
-                    min-w-[84px]
-                    h-[32px] 
-                    py-[4px] 
-                    px-[10px] 
-                    rounded-[8px] 
-                    flex 
-                    items-center 
-                    text-sm 
-                    font-medium 
-                    cursor-pointer 
-                    transition-all 
-                    outline-none 
-                    bg-[#9DA4B226] 
-                    text-gray-800 
-                    border-[0.5px] 
-                    border-transparent 
-                    justify-center
-                    ${isSelected ? `
-                        bg-white 
-                        border-[#E1E1E1] 
-                        justify-between 
-                        text-[#1C1C1C]
-                    ` : `
-                        hover:bg-[#9DA4B259]
-                        group-focus-within:bg-white
-                        group-focus-within:shadow-[0px_1px_3px_0px_rgba(0,0,0,0.04),_0px_1px_1px_0px_rgba(0,0,0,0.02),_0px_0px_0px_1.5px_rgba(47,114,226,0.25)]
-                        group-focus-within:justify-center
-                    `
-                    }
-                `}
-            >
+            <div className={`${defaultClasses} ${isSelected ? selectedStateClasses : focusStateClasses}`} >
                 <div className="flex items-center gap-2">
                     <Icon
                         className={`
